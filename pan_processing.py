ae986210ea434fcb7380a13e3f827601778a4cb4
chidiugonna <chidi_ugonna@hotmail.com>
2023-10-04 21:33:13 -0700

    Start versioning at v0.1.0

--- pan_processing.py
+++ pan_processing.py
@@ -9,7 +9,7 @@ import os
 import logging
 import sys
 
-__version__="0.2.0"
+__version__="0.1.0"
 
 datelabel = datetime.datetime.now().strftime("%Y%m%d_%H%M%S_%f")
 logger = logging.getLogger()
--- single_subject.py
+++ single_subject.py
@@ -11,7 +11,7 @@ import datetime
 import logging
 import sys
 
-__version__="0.2.0"
+__version__="0.1.0"
 
 panFactory = Factory.getPANFactory()
 
