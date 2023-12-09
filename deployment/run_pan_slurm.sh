c9728f261e3098817eae77fc076d249cd87c3dee
chidiugonna <chidi_ugonna@hotmail_com>
2023-12-09 11:50:12 -0700

    Refine deployment notes and scripts

--- deployment/run_pan_slurm.sh
+++ deployment/run_pan_slurm.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# Add commands here to start your python environment if required
+# 1. Add commands here to start your python environment if required
 # e.g. module load python/3.8/3.8.2 or conda activate [ENVNAME]
-
 # module load python/3.8/3.8.2
 
-# Change PYTHON=python3 if this is required to access python version.
+# 2. Change PYTHON=python3 if this is required to access python version.
 PYTHON=python
 
 PKG_DIR="/xdisk/nkchen/chidiugonna/PANpipelines/src"
 LOCPY=${PKG_DIR}/panpipelines
 
-# Export python path in case 'panpipelines' python package not accessible. This should not be necessary if
-# panpipelines has been installed in yout python environment
-#export PYTHONPATH=${PKG_DIR}:$PYTHONPATH
+# 3. Export python path in case 'panpipelines' python package not accessible. This should not be necessary if
+# panpipelines has been installed in yout python environment. Just uncomment line below.
+# export PYTHONPATH=${PKG_DIR}:$PYTHONPATH
 
 CONFIG=$PWD/config/panpipeconfig_slurm.config
 
