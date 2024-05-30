2e970973f9c2bb621bbff097671b6e068edbcec1
chidiugonna <chidi_ugonna@hotmail.com>
2024-05-30 09:17:23 -0700

    Add mriqc pipeline

---
+++ src/panpipelines/workflows/mriqc_workflow.py
@@ -0,0 +1,26 @@
+from nipype import Workflow
+
+import panpipelines.nodes.mriqc as mriqc
+from panpipelines.utils.util_functions import *
+
+
+def create(name, wf_base_dir,labels_dict,createGraph=True,execution={},LOGGER=None):
+    # Create workflow
+    pan_workflow = Workflow(name=name, base_dir=wf_base_dir)
+
+    if LOGGER:
+        LOGGER.info(f"Created Workflow {name} with base directory {wf_base_dir}")
+
+    if len(execution.keys()) > 0:
+        pan_workflow.config = process_dict(pan_workflow.config,execution)
+
+    # Specify node inputs
+    mriqc_node = mriqc.create(labels_dict,LOGGER=LOGGER)
+
+    pan_workflow.add_nodes([mriqc_node])
+
+    if createGraph:
+         pan_workflow.write_graph(graph2use='flat')
+
+
+    return pan_workflow
