d5d3d48d334605c6b499ea7ef216ebcdd8b69b7b
chidiugonna <chidi_ugonna@hotmail.com>
2024-05-30 10:34:33 -0700

    remove extraneous code

--- src/panpipelines/workflows/collatecsv_workflow.py
+++ src/panpipelines/workflows/collatecsv_workflow.py
@@ -16,31 +16,21 @@ def create(name, wf_base_dir,labels_dict,createGraph=True,execution={}, LOGGER=N
     if len(execution.keys()) > 0:
         pan_workflow.config = process_dict(pan_workflow.config,execution)
 
+    filecount_list=[]
     measures_list1=[]
-    measures_template = getParams(labels_dict,"MEASURES_TEMPLATE1")
+    measures_template = getParams(labels_dict,"MEASURES_TEMPLATE")
     if isinstance(measures_template,list):
         for meas_template in measures_template:
             evaluated_meas_template = substitute_labels(meas_template,labels_dict)
-            measures_list1.extend(glob.glob(evaluated_meas_template))
+            meas_files=glob.glob(evaluated_meas_template)
+            measures_list1.extend(meas_files)
+            filecount_list.append(len(meas_files))
     elif measures_template:
         measures_list1.extend(glob.glob(measures_template))
 
-    measures_list1.sort()
 
-    measures_list2=[]
-    measures_template = getParams(labels_dict,"MEASURES_TEMPLATE2")
-    if isinstance(measures_template,list):
-        for meas_template in measures_template:
-            evaluated_meas_template = substitute_labels(meas_template,labels_dict)
-            measures_list2.extend(glob.glob(evaluated_meas_template))
-    elif measures_list2:
-        measures_list2.extend(glob.glob(measures_template))
-
-    measures_list2.sort()
-
-    collate_csv_node = collate_csv.create(labels_dict, csv_list1=measures_list1, csv_list2=measures_list2, LOGGER=LOGGER)
+    collate_csv_node = collate_csv.create(labels_dict, csv_list1=measures_list1, LOGGER=LOGGER)
     pan_workflow.add_nodes([collate_csv_node])
-
 
     if createGraph:
          pan_workflow.write_graph(graph2use='flat')
