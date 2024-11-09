211498120c28dd3cfda5dfdbd0701e8f5bc600ff
chidiugonna <chidi_ugonna@hotmail.com>
2024-11-08 17:54:02 -0700

    Manage UM change to protocol - trim 1st volumes of M0 and ASL

--- src/panpipelines/nodes/aslprep.py
+++ src/panpipelines/nodes/aslprep.py
@@ -11,11 +11,14 @@ IFLOGGER=nlogging.getLogger('nipype.interface')
 
 IS_PRESENT="^^^"
 IGNORE="###"
-
+ 
 def aslprep_proc(labels_dict,bids_dir=""):
 
     cwd=os.getcwd()
     labels_dict = updateParams(labels_dict,"CWD",cwd)
+
+    if not bids_dir:
+        bids_dir = substitute_labels(labels_dict,"<BIDS_DIR>")
 
     command_base, container = getContainer(labels_dict,nodename="aslprep",SPECIFIC="ASLPREP_CONTAINER",LOGGER=IFLOGGER)
     TEMPLATEFLOW_HOME=getParams(labels_dict,"TEMPLATEFLOW_HOME")
@@ -29,6 +32,24 @@ def aslprep_proc(labels_dict,bids_dir=""):
     
     participant_label = getParams(labels_dict,'PARTICIPANT_LABEL')
     participant_session = getParams(labels_dict,'PARTICIPANT_SESSION')
+    participant_project = getParams(labels_dict,"PARTICIPANT_XNAT_PROJECT")
+
+    UM_EXCEPTION = False
+    if participant_project == "002_HML":
+        part_df = pd.read_csv(os.path.join(bids_dir,"participants.tsv"),sep="\t")
+        search_df = part_df[(part_df["participant_id"]=="sub-" + drop_sub(participant_label)) & (part_df["session_id"] == "ses-" + drop_ses(participant_session))]
+        if not search_df.empty:
+            scantime = search_df.iloc[0]["mri_scan_datetime"]
+            if datetime.datetime.strptime(scantime,"%Y-%m-%dT%H:%M:%S.%f%Z") > datetime.datetime(2024,10,21):
+                UM_EXCEPTION = True
+
+    if UM_EXCEPTION:
+        new_bids_dir = os.path.join(cwd,"bids_dir")
+        copytree(os.path.join(bids_dir,f"sub-{participant_label}"),os.path.join(new_bids_dir,f"sub-{participant_label}"))
+        copy(os.path.join(bids_dir,"participants.tsv"),os.path.join(new_bids_dir,"participants.tsv"))
+        copy(os.path.join(bids_dir,"dataset_description.json"),os.path.join(new_bids_dir,"dataset_description.json"))
+        bids_dir = new_bids_dir
+        process_um_exception(bids_dir, cwd, participant_label, participant_session,labels_dict)
 
     aslprep_dict={}
     aslprep_dict = updateParams(aslprep_dict,"--participant_label","<PARTICIPANT_LABEL>")
@@ -63,7 +84,7 @@ def aslprep_proc(labels_dict,bids_dir=""):
             print(f"aslprep tag {aslprep_tag} not valid.") 
 
     command = f"{command_base}"\
-        " <BIDS_DIR>"\
+        f" {bids_dir}"\
         " <CWD>"\
         " participant"\
         " " + params
