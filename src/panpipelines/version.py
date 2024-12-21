9981b2740d52b585dff92bba7de74d4f6496fcbc
chidiugonna <chidi_ugonna@hotmail.com>
2024-12-20 21:04:52 -0700

    Bump version to 1.0.8

--- src/panpipelines/version.py
+++ src/panpipelines/version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.0.7'
-__version_tuple__ = version_tuple = (1, 0, 7)
+__version__ = version = '1.0.8'
+__version_tuple__ = version_tuple = (1, 0, 8)
